6952e7807c98eba00da99a6a5f1f14158bc34ad7
Florens Verschelde <florens@fvsch.com>
2022-09-29 11:26:42 +0200

    Create new home page (with dummy content so far) (#8)

--- docs/.vitepress/theme/components/CardLink.vue
+++ docs/.vitepress/theme/components/CardLink.vue
@@ -1,45 +1,55 @@
-$bp-medium: 680px;
-$bp-large: 1000px;
+<script setup lang="ts">
+import { computed } from 'vue';
 
-.GridLinkGrid {
-  display: grid;
-  max-width: 1280px;
-  margin: 48px auto;
-  padding: 0 24px;
-  gap: 24px;
-  list-style: none;
+const props = defineProps<{
+  bgImgLight?: string;
+  bgImgDark?: string;
+  description: string;
+  icon?: 'embed' | 'import' | 'sdk';
+  large?: boolean;
+  title: string;
+  url: string;
+}>();
 
-  @media (min-width: $bp-medium) {
-    grid-template-columns: 1fr 1fr;
-  }
-  @media (min-width: $bp-large) {
-    grid-template-columns: repeat(6, 1fr);
-    .GridLinkItem {
-      grid-column: span 2;
-    }
-    .GridLinkItem--large {
-      grid-column: span 3;
-    }
-  }
-}
+const hasBackgroundImage = computed(() => Boolean(props.bgImgDark && props.bgImgLight));
+const backgroundStyle = computed(() => ({
+  '--bg-img-dark': `url(${props.bgImgDark})`,
+  '--bg-img-light': `url(${props.bgImgLight})`,
+}));
+</script>
 
-.GridLink {
+<template>
+  <a
+    :class="{ CardLink: true, 'CardLink--large': large, 'CardLink--bg': hasBackgroundImage }"
+    :style="hasBackgroundImage ? backgroundStyle : undefined"
+    :href="url"
+  >
+    <strong class="CardLink-title" :data-icon="icon">
+      {{ title }}
+    </strong>
+    <span class="CardLink-description">
+      {{ description }}
+    </span>
+  </a>
+</template>
+
+<style scoped lang="scss">
+@import './vars';
+
+.CardLink {
+  --icon-color: var(--vp-c-brand-lighter);
+  --icon-color: var(--sb-foreground-highlight);
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   min-height: 100%;
   border-radius: 8px;
-  border: solid 2px var(--border-color, transparent);
+  border: solid 2px hsl(200 30% 40% / 0.15);
   padding: 20px 24px 24px;
   // Bit of a specificity battle going on with theme styles
-  color: var(--text-color, inherit);
+  color: var(--vp-c-text-2);
   text-decoration: none;
-  background-color: var(--bg-color, transparent);
-  background-image: var(--bg-img, none);
-  background-origin: border-box;
-  background-position: left center;
-  background-repeat: no-repeat;
-  background-size: cover;
+  background-color: var(--vp-c-bg);
   transition: box-shadow 0.1s ease, transform 0.1s ease;
   transform: translateZ(0); // fixes opacity flicker when other elements are hovered :|
 
@@ -51,45 +61,34 @@ $bp-large: 1000px;
     border-radius: 12px;
   }
 
-  // Light style
-  :root:not(.dark) &:not(.GridLink--invert),
-  :root.dark &.GridLink--invert {
-    --title-color: #0c0f12;
-    --title-border-color: var(--vp-c-brand-light);
-    --text-color: #171e23;
-    --bg-color: #ffffff;
-    --bg-img: var(--bg-img-light);
-    --icon-color: var(--vp-c-brand-lighter);
-    --border-color: hsla(200, 30%, 40%, 0.15);
-    --border-hover-color: var(--vp-c-brand-lighter);
-  }
-
-  // Dark style
-  :root.dark &:not(.GridLink--invert),
-  :root:not(.dark) &.GridLink--invert {
-    --title-color: #ffffff;
-    --title-border-color: var(--vp-c-brand-light);
-    --text-color: #ffffff;
-    --bg-color: hsl(220 14% 13%);
-    --bg-img: var(--bg-img-dark);
-    --icon-color: var(--vp-c-brand-lighter);
-    --border-color: hsla(200, 30%, 40%, 0.15);
-    --border-hover-color: var(--vp-c-brand-lighter);
-  }
-
   &:hover {
     --icon-color: currentColor !important;
-    border-color: var(--border-hover-color);
+    border-color: var(--sb-foreground-highlight-high);
     transform: translateY(-2px);
   }
 
   &:focus-visible {
-    outline: solid 2px var(--border-hover-color);
+    --icon-color: currentColor !important;
+    outline: solid 2px var(--sb-foreground-highlight-high);
     outline-offset: -2px;
   }
 }
 
-.GridLink-title {
+.CardLink--bg {
+  background-origin: border-box;
+  background-position: left center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  :root:not(.dark) & {
+    background-image: var(--bg-img-light);
+  }
+  :root.dark & {
+    background-image: var(--bg-img-dark);
+  }
+}
+
+.CardLink-title {
   display: flex;
   align-items: center;
   position: relative;
@@ -99,7 +98,7 @@ $bp-large: 1000px;
   font-size: 16px;
   font-weight: 600;
   letter-spacing: 0.1px;
-  color: var(--title-color, currentColor);
+  color: var(--vp-c-text-1);
 
   &[data-icon='embed'] {
     --icon-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" viewBox="0 0 14 18"><path d="M5.4651 12.2742L5.45781 12.2671L4.26198 11.2513L5.45781 10.2355C5.58906 10.123 5.60365 9.92619 5.48698 9.79965L5.47969 9.79262L4.84531 9.13884C4.72135 9.0123 4.51719 9.00879 4.39323 9.12478L2.28958 11.0264C2.15469 11.1494 2.15469 11.3568 2.28958 11.4763L4.39323 13.3779C4.45156 13.4306 4.53177 13.4622 4.61198 13.4622C4.69948 13.4622 4.78698 13.4271 4.84531 13.3638L5.47969 12.71C5.6 12.587 5.59271 12.3902 5.4651 12.2742ZM13.4859 3.44464L10.4271 0.495606C10.099 0.179262 9.65417 0 9.19115 0H1.75C0.783854 0.00351494 0 0.759227 0 1.69069V16.3128C0 17.2443 0.783854 18 1.75 18H12.25C13.2161 18 14 17.2443 14 16.3128V4.63972C14 4.19332 13.8141 3.76098 13.4859 3.44464ZM9.33333 1.82777L12.1078 4.50264H9.33333V1.82777ZM12.25 16.3128H1.75V1.69069H7.58333V5.34622C7.58333 5.81371 7.97344 6.18981 8.45833 6.18981H12.25V16.3128ZM7.64167 7.52548C7.47031 7.47627 7.29531 7.57118 7.24427 7.73638L5.25 14.348C5.19896 14.5132 5.2974 14.6854 5.46875 14.7311L6.35833 14.9807C6.52969 15.0299 6.70833 14.9315 6.75573 14.7698L8.75 8.15466C8.80104 7.98946 8.7026 7.81722 8.53125 7.77153L7.64167 7.52548ZM8.5349 10.2285L8.54219 10.2355L9.73802 11.2513L8.54219 12.2671C8.41094 12.3796 8.39635 12.5765 8.51302 12.703L8.52031 12.71L9.15469 13.3638C9.275 13.4868 9.47917 13.4939 9.60677 13.3779L11.7104 11.4763C11.8453 11.3533 11.8453 11.1459 11.7104 11.0264L9.60677 9.12478C9.47917 9.00879 9.275 9.0123 9.15469 9.13884L8.52031 9.79262C8.4 9.91564 8.40729 10.1125 8.5349 10.2285Z"/></svg>');
@@ -129,50 +128,50 @@ $bp-large: 1000px;
   }
 }
 
-.GridLink-description {
+.CardLink-description {
   display: block;
   max-width: 20em;
   margin: 0;
   font-size: 13px;
   font-weight: 500;
   line-height: 1.52;
-  opacity: 0.5;
-  transition: opacity 0.1s ease;
+  color: var(--vp-c-text-2);
+  transition: color 0.1s ease;
 
-  .GridLink:hover &,
-  .GridLink:focus & {
-    opacity: 0.8;
+  .CardLink:hover &,
+  .CardLink:focus & {
+    color: var(--vp-c-text-1);
   }
 }
 
 // Specific style for large cards,
 // on large screens only
 @media (min-width: $bp-large) {
-  .GridLink--large {
+  .CardLink--large {
     padding: 60px 72px;
 
-    .GridLink-title {
+    .CardLink-title {
       margin-bottom: 22px;
       padding-bottom: 5px;
       font-size: 24px;
       font-weight: 800;
       letter-spacing: 0;
     }
-    .GridLink-title::after {
+    .CardLink-title::after {
       content: '';
       position: absolute;
       bottom: 0;
       left: 0;
       width: 0;
       height: 2px;
-      background: var(--title-border-color, currentColor);
+      background-color: var(--vp-c-brand-light);
       transition: width 0.1s ease;
     }
-    &:hover .GridLink-title::after,
-    &:focus .GridLink-title::after {
+    &:hover .CardLink-title::after,
+    &:focus .CardLink-title::after {
       width: 100%;
     }
-    .GridLink-description {
+    .CardLink-description {
       // Reserve 4 lines
       min-height: calc(1.6 * 4em);
       font-size: 14px;
@@ -180,3 +179,4 @@ $bp-large: 1000px;
     }
   }
 }
+</style>
