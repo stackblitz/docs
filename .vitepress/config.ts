1d806fde89b0d39991782e3e1fdb6e194cd12f5b
Florens Verschelde <florens@fvsch.com>
2022-10-07 16:07:33 +0200

    Tweak settings for prod (#24)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,15 @@ import * as data from '../docs/data';
 import { GTAG_INIT, GTAG_URL } from './analytics';
 
 export default defineConfig({
+  srcDir: 'docs',
+  outDir: 'build',
+
+  // Generate files as `/path/to/page.html` and URLs as `/path/to/page`
+  cleanUrls: 'without-subfolders',
+
+  // Prevent builds when content has dead links
+  ignoreDeadLinks: false,
+
   // Metadata
   lang: 'en-US',
   title: 'StackBlitz Docs',
@@ -13,24 +22,14 @@ export default defineConfig({
     ['link', { rel: 'icon', type: 'image/png', href: '/img/favicon.png' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:title', content: 'StackBlitz Docs' }],
-    ['meta', { property: 'og:image', content: '/img/stackblitz-docs-social.png' }],
+    ['meta', { property: 'og:image', content: 'https://developer.stackblitz.com/img/stackblitz-docs-social.png' }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
     ['meta', { name: 'twitter:title', content: 'StackBlitz Docs' }],
     ['meta', { name: 'twitter:site', content: '@StackBlitz' }],
-    ['meta', { name: 'twitter:image', content: '/img/stackblitz-docs-social.png' }],
-    // TODO: remove before making public
-    ['meta', { name: 'robots', content: 'noindex,nofollow,noarchive' }],
+    ['meta', { name: 'twitter:image', content: 'https://developer.stackblitz.com/img/stackblitz-docs-social.png' }],
     ['script', { src: GTAG_URL, async: '' }],
     ['script', {}, GTAG_INIT],
   ],
-
-  srcDir: 'docs',
-  outDir: 'build',
-
-  // Build
-  cleanUrls: 'without-subfolders',
-  // Prevent builds when content has dead links
-  ignoreDeadLinks: false,
 
   // Theme
   themeConfig: {
@@ -41,10 +40,10 @@ export default defineConfig({
       apiKey: '8fe79cd865807082f50083ddd5647b0b',
       indexName: 'stackblitz',
     },
-    editLink: {
-     pattern: 'https://github.com/stackblitz/docs/edit/main/docs/:path',
-     text: 'Edit this page on GitHub',
-    },
+    //editLink: {
+    // pattern: 'https://github.com/stackblitz/docs/edit/main/docs/:path',
+    // text: 'Edit this page on GitHub',
+    //},
     nav: [
       { text: 'Guides', link: data.userGuideLinks[0].link },
       { text: 'API', link: data.apiLinks[0].link },
