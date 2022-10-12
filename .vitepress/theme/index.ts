56d4314f3e1f20968d1007ae4b230ac1d6b33106
Florens Verschelde <florens@fvsch.com>
2022-10-12 16:39:55 +0200

    Move edit link hack to a dedicated script module

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,14 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 
+import './scripts/edit-link';
 import './styles/index.scss';
-
-if (!!globalThis.window) {
-  document.body.addEventListener('click', function(e: any) {
-    if(e.target.classList.contains('edit-link-button')){
-      e.target.href = e.target.href + '?initialPath=' + document.location.pathname
-    }
-  }, true);
-}
 
 export default {
   ...DefaultTheme,
