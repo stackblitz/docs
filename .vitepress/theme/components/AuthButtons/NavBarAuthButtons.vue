9785f506e5afaed40410ea5b98bd408c111d7ada
ykmsd <yuka1110m@gmail.com>
2024-01-23 19:34:42 +0100

    Refactor

--- .vitepress/theme/components/AuthButtons/NavBarAuthButtons.vue
+++ .vitepress/theme/components/AuthButtons/NavBarAuthButtons.vue
@@ -1,31 +1,7 @@
+<script setup lang="ts">
+import AuthButtons from './AuthButtons.vue';
+</script>
+
 <template>
-    <div class="container">
-        <a href="https://stackblitz.com/sign_in" class="link light">Sign in</a>
-        <a href="https://stackblitz.com/register" class="link accent">Register</a>
-    </div>
+  <AuthButtons place="navBar" />
 </template>
-
-<style scoped lang="scss">
-@import './linkStyles.scss';
-
-.container {
-    margin-inline-start: 40px;
-    display: flex;
-    gap: 8px;
-    // this is when we hide top nav
-    @media screen and (max-width: 768px) {
-      margin-inline-start: 0px;
-    }
-    // we show the buttons inside the hamburger menu when on smaller screen
-    @media screen and (max-width: 400px) {
-      display: none;
-    }
-}
-
-.link {
-    @media screen and (max-width: 500px) {
-      height: 30px;
-      padding: 0 10px;
-    }
-}
-</style>
--- .vitepress/theme/components/AuthButtons/NavScreenAuthButtons.vue
+++ .vitepress/theme/components/AuthButtons/NavScreenAuthButtons.vue
@@ -1,25 +1,7 @@
+<script setup lang="ts">
+import AuthButtons from './AuthButtons.vue';
+</script>
+
 <template>
-    <div class="container">
-        <a href="https://stackblitz.com/sign_in" class="link light">Sign in</a>
-        <a href="https://stackblitz.com/sign_in" class="link accent">Register</a>
-    </div>
+  <AuthButtons place="navScreen" />
 </template>
-
-<style scoped lang="scss">
-@import './linkStyles.scss';
-
-.container {
-    // We show the buttons on nav bar when there is enough space
-    display: none;
-    @media screen and (max-width: 400px) {
-      display: flex;
-      gap: 8px;
-      margin-block-start: 24px;
-    }
-}
-
-.link {
-  flex: 1;
-  justify-content: center;
-}
-</style>
