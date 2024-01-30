b05a9f1fb2bc1eb24e3c3f0b469903dc10dedd6d
Yuka Masuda <yuka1110m@gmail.com>
2024-01-30 13:44:16 +0100

    feat: add auth buttons  (#196)
    * Add auth buttons
    
    * Adjust for light theme
    
    * Adjust for mobile
    
    * Cleanup and add comments
    
    * Refactor
    
    * Fix path
    
    * Update .vitepress/theme/components/AuthButtons/AuthButtons.vue
    
    Co-authored-by: Gabriel Daltoso <ggdaltoso@gmail.com>
    
    ---------
    
    Co-authored-by: null <null>
    Co-authored-by: Gabriel Daltoso <ggdaltoso@gmail.com>

--- .vitepress/theme/components/CustomLayout.vue
+++ .vitepress/theme/components/CustomLayout.vue
@@ -5,15 +5,23 @@ const { Layout } = DefaultTheme;
 
 import Feedback from '@theme/components/Feedback/Feedback.vue';
 import Chatlio from '@theme/components/Chatlio.vue';
+import NavBarAuthButtons from '@theme/components/AuthButtons/NavBarAuthButtons.vue';
+import NavScreenAuthButtons from '@theme/components/AuthButtons/NavScreenAuthButtons.vue';
 </script>
 
 <template>
   <Layout>
+    <template #nav-bar-content-after>
+      <NavBarAuthButtons />
+    </template>
     <template #doc-footer-before>
       <ClientOnly>
         <Feedback />
         <Chatlio />
       </ClientOnly>
+    </template>
+    <template #nav-screen-content-after>
+      <NavScreenAuthButtons />
     </template>
   </Layout>
 </template>
